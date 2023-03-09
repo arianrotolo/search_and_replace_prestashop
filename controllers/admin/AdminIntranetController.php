cc2264ddcb639021f8bc95611902d890bf0b5ee5
Arian Rotolo <arianrotolo@gmail.com>
2023-03-09 15:26:34 +0100

    Version 1.0.1: Busca la ruta desde el input

--- controllers/admin/AdminIntranetController.php
+++ controllers/admin/AdminIntranetController.php
@@ -15,9 +15,12 @@ class AdminIntranetController extends ModuleAdminController
     public function postProcess()
     {
         // Ruta del archivo a modificar
-        $file_path = '/home/vestatex/pruebas.piscihogar.com/prueba/ejemplo.txt';
+        #$file_path = '/home/vestatex/pruebas.piscihogar.com/prueba/ejemplo.txt';
         #logDebugMessage("La ruta del archivo es: " + $file_path);
         if (Tools::isSubmit('submit')) {
+            // Obtener la ruta del archivo desde el input file-path
+            $file_path = Tools::getValue('file-path');
+            
             // Obtener los valores de los inputs del formulario
             $busqueda = Tools::getValue('buscar');
             $reemplazo = Tools::getValue('reemplazar');
@@ -45,5 +48,28 @@ class AdminIntranetController extends ModuleAdminController
             // Mostrar mensaje de éxito o error
             $this->confirmations[] = $this->l('Texto reemplazado correctamente');
         }
+    }
+
+    public function displayForm()
+    {
+        // Obtener la ruta de la carpeta de archivos
+        $folder_path = _PS_MODULE_DIR_ . 'mi_menu/';
+
+        // Obtener los archivos de la carpeta y sus rutas
+        $files = scandir($folder_path);
+        $file_list = array();
+        foreach ($files as $file) {
+            if ($file != '.' && $file != '..') {
+                $file_list[] = array(
+                    'name' => $file,
+                    'path' => $folder_path . $file
+                );
+            }
+        }
+
+        // Asignar los datos a la vista
+        $this->context->smarty->assign('files', $file_list);
+
+        return $this->display(__FILE__, 'views/templates/admin/intranet.tpl');
     }
 }
--- views/templates/admin/intranet.tpl
+++ views/templates/admin/intranet.tpl
@@ -1,4 +1,20 @@
 <form action="" method="post">
+
+    <div>
+        <label for="file-select">Seleccionar archivo:</label>
+        <select id="file-select" name="file-select">
+            <option value="">Seleccionar archivo...</option>
+            {foreach $files as $file}
+            <option value="{$file['path']}">{$file['name']}</option>
+            {/foreach}
+        </select>
+    </div>
+    <br/>
+    <div>
+        <label for="file-path">Ruta del archivo:</label>
+        <input type="text" id="file-path" name="file-path" value="/home/vestatex/pruebas.piscihogar.com/prueba/ejemplo.txt">
+    </div>
+
     <div>
         <label for="buscar">Buscar:</label>
         <input type="text" name="buscar" id="buscar" />
@@ -12,7 +28,6 @@
     </div>
 </form>
 <!-- Agrega una etiqueta HTML para mostrar la información de depuración -->
-
 <div>
     <pre>{print_r($debug_info)}</pre>
 </div>
